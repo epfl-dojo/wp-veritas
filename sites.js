51e5d693bb3fab238edc74c695b048d64a850747
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2019-01-11 16:59:27 +0100

    Insertion de données bidon si pas en prod et 0 entrées

--- sites.js
+++ sites.js
@@ -2,5 +2,13 @@ import { Mongo } from 'meteor/mongo';
 
 export const Sites = new Mongo.Collection('sites');
 
-Meteor.startup(() => {
+Meteor.startup(async function() {
+  let sites = await Sites.find({}).fetch();
+  if (process.env.NODE_ENV != "production" &&
+    ! (sites && sites.length)) {
+    console.log("Insertion de données bidon...");
+    return Sites.insert(
+      {"url":"http://toto.epfl.ch","tagline":"aaa","title":"titre","openshift_env":"www","type":"private","theme":"2018","faculty":"CDH","language":"en","unit_id":"123","snow_number":"123"}
+    );
+  }
 })
